19c9078fd5f7119e296f618a300da1f105c04808
Justin Cañedo <111473062+Myles09@users.noreply.github.com>
2022-09-05 08:40:37 +0800

    APP CHARACTER

---
+++ Character.java
@@ -0,0 +1,29 @@
+public class Character {
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
+
+    public void sayMyName() {
+        System.out.println(" Hello I am " + name);
+    }
+    
+        public void sayMyStrength() {
+       System.out.println(" My Strength is " + strength);
+    }
+       public void sayMyAgility() {
+        System.out.println(" My Agility is " + agility);
+    } 
+        public void sayMyIntelligence() {
+            System.out.println(" My Intelligence is " + intelligence); 
+
+
+   }       
+   public Character(int strength, int agility, int intelligence, String name){
+   strength = this.strength;
+   agility = this.agility;
+   intelligence = this.intelligence;
+   name = this.name;
+}         
+} 
+   
